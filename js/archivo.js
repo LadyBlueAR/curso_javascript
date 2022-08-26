257c15c8a7f5f553931b7ac53245eb1df10585b4
Anabela Gemignani <anabelag1991@gmail.com>
2022-08-25 23:01:29 -0300

    Alert

--- js/archivo.js
+++ js/archivo.js
@@ -3,8 +3,8 @@ let contador = parseInt(prompt("Dime hasta que número quieres que cuente: "))
 
 for (let i = 1; i <= contador ; i ++) {
     if (i > 10) {
-        console.warn("¡Lo siento!, solo sé contar hasta 10 :(")
+        alert("¡Lo siento!, solo sé contar hasta 10 :(")
         break
     }
-    console.log(i)
+    alert(i)
 }
